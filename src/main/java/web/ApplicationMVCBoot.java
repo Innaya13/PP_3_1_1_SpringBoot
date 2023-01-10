55aba0ca144e35ebceee79b78ff3359989255783
mac <innakar@gmail.com>
2023-01-10 18:57:41 -0500

    changes

--- src/main/java/web/ApplicationMVCBoot.java
+++ src/main/java/web/ApplicationMVCBoot.java
@@ -4,8 +4,9 @@ import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ApplicationMVCBoot {
 
     public static void main(String[] args) {
